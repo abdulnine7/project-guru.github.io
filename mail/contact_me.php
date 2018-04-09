02e9c578a019a1b47fe52ddedb9a47adbfa96561
Abdul <nav20202@gmail.com>
2018-04-09 16:08:29 +0530

    Added SMS response on registration success.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -44,5 +44,36 @@ fclose($myfile);
 
 //Sending mail
 mail($to,$email_subject,$email_body,$headers);
+
+//Sending SMS
+$curl = curl_init();
+
+$message = "Hey there ". $name .",\nYour request has been received. We will reach you shortly. Stay updated with your mails.\n\n~Project Guru";
+
+$message=urlencode($message);
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://api.msg91.com/api/sendhttp.php?sender=PRJGRU&route=4&mobiles=" . $phone . "&authkey=196448A7QYpXEK5acb0425&country=0&message=" . $message,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_SSL_VERIFYHOST => 0,
+  CURLOPT_SSL_VERIFYPEER => 0,
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
 return true;			
 ?>
