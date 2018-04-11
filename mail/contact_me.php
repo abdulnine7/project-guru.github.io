295086120b0823df35477d72bbe7d360b1d8657c
Abdul <nav20202@gmail.com>
2018-04-11 23:30:02 +0530

    Updated contact_me and index

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -29,7 +29,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'bitwiseabdul@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Project Guru - Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nCollege: $college\n\nBranch: $branch\n\nYear: $year\n\nType: $type\n\nCategory: $category\n\nMessage:\n$message";
-$headers = "From: noreply@project-guru.ml\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: ProjectGuru@project-guru.ml\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 
 
